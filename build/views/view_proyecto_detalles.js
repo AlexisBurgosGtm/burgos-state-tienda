4894394da6a183daf7b150590f53811d5f6c48f6
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2024-01-25 12:47:38 -0600

    a

--- build/views/view_proyecto_detalles.js
+++ build/views/view_proyecto_detalles.js
@@ -34,75 +34,10 @@ function card_proyecto(codigo,nombre,direccion,municipio,latitud,longitud,desde,
     let card = '';
 
     let tipodispositivo = F.detectarPc();
-    if(tipodispositivo=='tel'){
+    console.log('dispositivo:')
+    console.log(tipodispositivo)
+    if(tipodispositivo=='PC'){
         card = `
-        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-            <div class="card card-rounded border-especial shadow p-4" id="card_detalle">
-                <div class="card-body bg-white text-center">
-                    <img src="${foto_logo}" width="60px" height="45px">
-                    <h4 class="text-danger">${nombre}</h4>
-                    <small class="text-secondary">${direccion}, ${municipio}</small>
-                    <br>
-                    <small class="text-info">Precios desde ${funciones.setMoneda(desde,'Q')}</small>
-                    <hr class="solid">
-                    <div class="row">
-                        <div class="col-4">
-                            <button class="btn btn-success hand col-12" onclick="F.gotoGoogleMaps('${latitud}','${longitud}')">
-                                <i class="lni lni-map-marker"></i> GMaps
-                            </button>
-                        </div>
-                        <div class="col-4">
-                            <button class="btn btn-info hand col-12" onclick="F.gotoWaze('${latitud}','${longitud}')">
-                                <i class="lni lni-map-marker"></i> WAZE
-                            </button>
-                        </div>
-                        <div class="col-4">
-                            <a class="animate__animated animate__zoomInLeft btn btn-danger hand col-12" href="https://api.whatsapp.com/send/?phone=50257255092&text=Hola+Burgos+State+%21%21+quisiera+informaci%C3%B3n+sobre+${nombre}%21%21&type=phone_number&app_absent=0" target="_blank">
-                                <i class="lni lni-whatsapp"></i> info
-                            </a>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
-            <div class="card card-rounded border-especial shadow p-4" id="card_detalle2">
-                
-                <div class="card-body">
-                    <div class="row p-2">
-                        <div class="col-12" id="cp_intro" >
-                            <img width="100%" height="100%" class="card-rounded shadow" 
-                            src="${foto_portada}" onclick="F.expandir('cp_intro')">
-                        </div>
-                    </div>
-
-                    <div class="row p-2">
-                        <div class="col-6" id="cp_1">
-                            <img class="card-rounded shadow" src="${foto_uno}" onclick="F.expandir('cp_1')">
-                        </div>
-                        <div class="col-6" id="cp_2">
-                            <img class="card-rounded shadow" src="${foto_dos}" onclick="F.expandir('cp_2')">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <iframe width="560" height="315" 
-                        src="${foto_video}" 
-                        title="YouTube video player" frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; 
-                        encrypted-media; gyroscope; picture-in-picture; web-share" 
-                        allowfullscreen></iframe>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-        `;
-    }else{
-        card = `
-    
         <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
             <div class="card card-rounded border-especial shadow p-4" id="card_detalle">
                 <div class="card-body bg-white text-center">
@@ -179,8 +114,76 @@ function card_proyecto(codigo,nombre,direccion,municipio,latitud,longitud,desde,
 
     
         `;
+    }else{
+        card = `
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <div class="card card-rounded border-especial shadow p-4" id="card_detalle">
+                <div class="card-body bg-white text-center">
+                    <img src="${foto_logo}" width="60px" height="45px">
+                    <h4 class="text-danger">${nombre}</h4>
+                    <small class="text-secondary">${direccion}, ${municipio}</small>
+                    <br>
+                    <small class="text-info">Precios desde ${F.setMoneda(desde,'Q')}</small>
+                    <hr class="solid">
+                    <div class="row">
+                        <div class="col-4">
+                            <button class="btn btn-success hand col-12" onclick="F.gotoGoogleMaps('${latitud}','${longitud}')">
+                                <i class="lni lni-map-marker"></i> GMaps
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <button class="btn btn-info hand col-12" onclick="F.gotoWaze('${latitud}','${longitud}')">
+                                <i class="lni lni-map-marker"></i> WAZE
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <a class="animate__animated animate__zoomInLeft btn btn-danger hand col-12" href="https://api.whatsapp.com/send/?phone=50257255092&text=Hola+Burgos+State+%21%21+quisiera+informaci%C3%B3n+sobre+${nombre}%21%21&type=phone_number&app_absent=0" target="_blank">
+                                <i class="lni lni-whatsapp"></i> info
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+            <div class="card card-rounded border-especial shadow p-4" id="card_detalle2">
+                
+                <div class="card-body">
+                    <div class="row p-2">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_portada}" onclick="F.expandir('cp_intro')">
+                        </div>
+                    </div>
+
+                    <div class="row p-2">
+                        <div class="col-6" id="cp_1">
+                            <img class="card-rounded shadow" src="${foto_uno}" onclick="F.expandir('cp_1')">
+                        </div>
+                        <div class="col-6" id="cp_2">
+                            <img class="card-rounded shadow" src="${foto_dos}" onclick="F.expandir('cp_2')">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <iframe width="560" height="315" 
+                        src="${foto_video}" 
+                        title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; 
+                        encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowfullscreen></iframe>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        `;
     }
     
     return card;
+
+
 
 };
