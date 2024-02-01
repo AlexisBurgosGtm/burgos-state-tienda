5690c5710e69b5bb7fdc1314b2b76b8eb3a90e8d
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2024-02-01 10:22:30 -0600

    varios

--- build/views/view_proyecto_detalles.js
+++ build/views/view_proyecto_detalles.js
@@ -12,6 +12,11 @@ function get_detalles_proyecto(data){
 
     location.hash = "#card_detalle";
     location.hash = '';
+    
+    $('.carousel').carousel({
+        interval: 1000
+      })
+
 };
 
 function get_detalles_proyecto_card(codigo,nombre,direccion,municipio,latitud,longitud,desde,area,foto_logo,foto_portada,foto_uno,foto_dos,foto_video){
@@ -26,10 +31,15 @@ function get_detalles_proyecto_card(codigo,nombre,direccion,municipio,latitud,lo
 
     location.hash = "#card_detalle";
     location.hash = '';
+
+    
+    $('.carousel').carousel({
+        interval: 1000
+      })
 };
 
 
-function card_proyecto(codigo,nombre,direccion,municipio,latitud,longitud,desde,area,foto_logo,foto_portada,foto_uno,foto_dos,foto_video){
+function BACKUP_card_proyecto(codigo,nombre,direccion,municipio,latitud,longitud,desde,area,foto_logo,foto_portada,foto_uno,foto_dos,foto_video){
 
     let card = '';
 
@@ -187,3 +197,177 @@ function card_proyecto(codigo,nombre,direccion,municipio,latitud,longitud,desde,
 
 
 };
+
+function card_proyecto(codigo,nombre,direccion,municipio,latitud,longitud,desde,area,foto_logo,foto_portada,foto_uno,foto_dos,foto_video){
+
+    let card = '';
+
+    let tipodispositivo = F.detectarPc();
+    console.log('dispositivo:')
+    console.log(tipodispositivo)
+    if(tipodispositivo=='PC'){
+        card = `
+        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <div class="card card-rounded border-especial shadow p-2" id="card_detalle">
+                <div class="card-body bg-white text-center">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${foto_logo}" width="110px" height="85px">
+                        </div>
+                        <div class="col-9">
+                            <h4 class="text-danger">${nombre}</h4>
+                            <small class="text-secondary">${direccion}, ${municipio}</small>
+                            <br>
+                            <small class="text-info negrita">Precios desde ${F.setMoneda(desde,'Q')}</small>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <br>
+                        <small class="text-danger negrita">Agenda tu visita, será un enorme gusto llevarte personalmente a conocer la mejor opción para ti!!</small>
+                    </div>
+                    
+                    
+                    <hr class="solid">
+                    <div class="row">
+                        <div class="col-4">
+                            <button class="btn btn-success hand col-12" onclick="F.gotoGoogleMaps('${latitud}','${longitud}')">
+                                <i class="lni lni-map-marker"></i> GMaps
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <button class="btn btn-info hand col-12" onclick="F.gotoWaze('${latitud}','${longitud}')">
+                                <i class="lni lni-map-marker"></i> WAZE
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <a class="animate__animated animate__zoomInLeft btn btn-danger hand col-12" href="https://api.whatsapp.com/send/?phone=50239160727&text=Hola+Burgos+State+%21%21+quisiera+informaci%C3%B3n+sobre+${nombre}%21%21&type=phone_number&app_absent=0" target="_blank">
+                                <i class="lni lni-whatsapp"></i> info
+                            </a>
+                        </div>
+                    </div>
+                    <br><br>
+                    <div class="row">
+                        <iframe width="560" height="315" 
+                        src="${F.limpiarUrlYoutube(foto_video)}" 
+                        title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; 
+                        encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowfullscreen></iframe>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <div class="card card-rounded border-especial shadow p-2" id="card_detalle2">
+                
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_portada}">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_uno}">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_dos}">
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+
+    
+        `;
+    }else{
+        card = `
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <div class="card card-rounded border-especial shadow p-2" id="card_detalle">
+                <div class="card-body bg-white text-center">
+                    <img src="${foto_logo}" width="60px" height="45px">
+                    <h4 class="text-danger">${nombre}</h4>
+                    <small class="text-secondary">${direccion}, ${municipio}</small>
+                    <br>
+                    <small class="text-info">Precios desde ${F.setMoneda(desde,'Q')}</small>
+                    <hr class="solid">
+                    <div class="row">
+                        <div class="col-4">
+                            <button class="btn btn-success hand col-12" onclick="F.gotoGoogleMaps('${latitud}','${longitud}')">
+                                <i class="lni lni-map-marker"></i> GMaps
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <button class="btn btn-info hand col-12" onclick="F.gotoWaze('${latitud}','${longitud}')">
+                                <i class="lni lni-map-marker"></i> WAZE
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <a class="animate__animated animate__zoomInLeft btn btn-danger hand col-12" href="https://api.whatsapp.com/send/?phone=50239160727&text=Hola+Burgos+State+%21%21+quisiera+informaci%C3%B3n+sobre+${nombre}%21%21&type=phone_number&app_absent=0" target="_blank">
+                                <i class="lni lni-whatsapp"></i> info
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+            <div class="card card-rounded border-especial shadow p-2" id="card_detalle2">
+                
+                <div class="card-body">
+                   
+                    <div class="row">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_portada}">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_uno}">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-12" id="cp_intro" >
+                            <img width="100%" height="100%" class="card-rounded shadow" 
+                            src="${foto_dos}">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <iframe width="560" height="315" 
+                        src="${F.limpiarUrlYoutube(foto_video)}" 
+                        title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; 
+                        encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowfullscreen></iframe>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    
+    return card;
+
+
+
+};
+
